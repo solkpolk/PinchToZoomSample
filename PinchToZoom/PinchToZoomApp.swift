8635817d87ca93368b58a2c5c0a6184c46ffb6a5
Alexander <solkpolk@gmail.com>
2023-11-16 15:43:39 -0500

    Initial Commit

---
+++ PinchToZoom/PinchToZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchToZoomApp.swift
+//  PinchToZoom
+//
+//  Created by Vybe Together on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchToZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
